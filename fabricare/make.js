324fba2d6c603f62670e27bb720744a20da0fdb4
Grigore Stefan <g_stefan@yahoo.com>
2022-12-13 13:22:29 +0200

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -3,18 +3,45 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
+messageAction("make");
+
 Shell.mkdirRecursivelyIfNotExists("output");
 Shell.mkdirRecursivelyIfNotExists("temp");
 
 // ---
 
-var defines = [
-	"_CRT_SECURE_NO_WARNINGS",
-	"XYO_OS_WINDOWS",
-	"XYO_COMPILER_MSVC",
+var defines = [];
+
+if(OS.isWindows()){
+	defines = defines.concat([
+		"_CRT_SECURE_NO_WARNINGS",
+		"XYO_OS_WINDOWS"
+	]);
+
+	if(Platform.name.indexOf("msvc")>=0){
+		defines = defines.concat([
+			"XYO_COMPILER_MSVC"
+		]);
+	};
+
+	if(Platform.name.indexOf("mingw")>=0){
+		defines = defines.concat([
+			"XYO_COMPILER_GCC"
+		]);
+	};
+};
+
+if(OS.isLinux()){
+	defines = defines.concat([
+		"XYO_OS_LINUX",
+		"XYO_COMPILER_GCC"
+	]);
+};
+
+defines = defines.concat([
 	"XYO_MULTI_THREAD",
 	"XYO_PLATFORM=\"" + Platform.name + "\""
-];
+]);
 
 if (Platform.machine == "x64") {
 	defines[defines.length] = "XYO_APPLICATION_64BIT";
@@ -35,7 +62,7 @@ compileAndRunTemp({
 
 // ---
 
-if (!Fabricare.include("msvc.make." + Project.make)) {
+if (!Fabricare.include("make." + Project.make)) {
 	messageError("Don't know how to make '" + Project.make + "'!");
 	exit(1);
 };
