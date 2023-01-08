a68e8d61c6ad26b49b4e8707026728e8f9c8cdd5
Grigore Stefan <g_stefan@yahoo.com>
2023-01-09 00:10:20 +0200

    Update

--- fabricare/platform/mingw32.js
+++
@@ -1,8 +0,0 @@
-// Created by Grigore Stefan <g_stefan@yahoo.com>
-// Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
-// SPDX-License-Identifier: Unlicense
-
-Platform.name = "mingw32";
-
-Fabricare.include("platform/mingw.subroutine");
--- fabricare/platform/sys-mingw64.js
+++
@@ -1,9 +0,0 @@
-// Created by Grigore Stefan <g_stefan@yahoo.com>
-// Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
-// SPDX-License-Identifier: Unlicense
-
-Platform.name = "sys-mingw64";
-Platform.next = "mingw64";
-
-Fabricare.include("platform/sys-mingw.subroutine");
