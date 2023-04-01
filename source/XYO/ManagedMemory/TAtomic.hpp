a7b81e44b10a85b60fbd60f4ec6954bbdf7d1f3b
Grigore Stefan <g_stefan@yahoo.com>
2023-04-02 02:01:19 +0300

    Update

--- source/XYO/ManagedMemory/TAtomic.hpp
+++ source/XYO/ManagedMemory/TAtomic.hpp
@@ -11,95 +11,54 @@
 #	include <XYO/ManagedMemory/Dependency.hpp>
 #endif
 
-#ifdef XYO_MULTI_THREAD
 namespace XYO::ManagedMemory {
 
 	template <typename T>
 	class TAtomic {
 		protected:
+
+		#ifdef XYO_MULTI_THREAD
 			std::atomic<T> value;
-
-		public:
-			static const bool isAtomic = true;
-
-			inline TAtomic(){};
-
-			inline void set(const T &value_) {
-				value.store(value_, std::memory_order_relaxed);
-			};
-
-			inline void set(T &&value_) {
-				value.store(value_, std::memory_order_relaxed);
-			};
-
-			inline T get() const {
-				return value.load(std::memory_order_relaxed);
-			};
-
-			inline TAtomic(const TAtomic &value_) {
-				set(value_.get());
-			};
-
-			inline TAtomic(TAtomic &&value_) {
-				set(value_.get());
-			};
-
-			inline TAtomic &operator=(const TAtomic &value_) {
-				set(value_.get());
-			};
-
-			inline TAtomic &operator=(TAtomic &&value_) {
-				set(value_.get());
-			};
-
-			inline TAtomic(const T &value_) {
-				set(value_);
-			};
-
-			inline TAtomic(T &&value_) {
-				set(value_);
-			};
-
-			inline TAtomic &operator=(const T &value_) {
-				set(value_);
-				return *this;
-			};
-
-			inline TAtomic &operator=(T &&value_) {
-				set(value_);
-				return *this;
-			};
-
-			inline operator T() const {
-				return get();
-			};
-	};
-};
-#endif
-
-#ifdef XYO_SINGLE_THREAD
-namespace XYO::ManagedMemory {
-
-	template <typename T>
-	class TAtomic {
-		protected:
+		#endif
+		#ifdef XYO_SINGLE_THREAD
 			T value;
+		#endif
 
 		public:
+		#ifdef XYO_MULTI_THREAD
+			static const bool isAtomic = true;
+		#endif
+		#ifdef XYO_SINGLE_THREAD
 			static const bool isAtomic = false;
+		#endif
 
 			inline TAtomic(){};
 
 			inline void set(const T &value_) {
+				#ifdef XYO_MULTI_THREAD
+				value.store(value_, std::memory_order_relaxed);
+				#endif
+				#ifdef XYO_SINGLE_THREAD
 				value = value_;
+				#endif
 			};
 
 			inline void set(T &&value_) {
+				#ifdef XYO_MULTI_THREAD
+				value.store(value_, std::memory_order_relaxed);
+				#endif
+				#ifdef XYO_SINGLE_THREAD
 				value = std::move(value_);
+				#endif
 			};
 
 			inline T get() const {
+				#ifdef XYO_MULTI_THREAD
+				return value.load(std::memory_order_relaxed);
+				#endif
+				#ifdef XYO_SINGLE_THREAD
 				return value;
+				#endif
 			};
 
 			inline TAtomic(const TAtomic &value_) {
@@ -141,6 +100,5 @@ namespace XYO::ManagedMemory {
 			};
 	};
 };
-#endif
 
 #endif
